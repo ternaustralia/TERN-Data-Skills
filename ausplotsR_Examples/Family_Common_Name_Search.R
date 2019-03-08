3d01d6c8fa7d0e198c2301664a3e8e889c4b3c5d
Sam Munroe <samantha.munroe@adelaide.edu.au>
2019-03-08 13:59:53 +1030

    edits to family search code

--- ausplotsR_Examples/Family_Common_Name_Search.R
+++ ausplotsR_Examples/Family_Common_Name_Search.R
@@ -2,20 +2,22 @@
 #########################################################
 ############Family & Common Name Search of TERN Database Species######## 
 #########################################################
+#Authors:Samantha Munroe
 
 #TERN does not record the family or common name of any species in our database
 #TERN records species data to the lowest possible taxonimic level (often genus-species, but can also include subsp.) 
-#However, in some cases, it would be useful to group TERN records based on other taxonmic information (such as family, order, common name, etc)
-#To do this, we need to collate a species list from the TERN database and search this list in the Atlas of Living Australia database (available via ALA4R)
+#To group TERN records based on other taxonmic information (such as family, order, common name, etc)
+#Collate a species list from the TERN database and search this list in the Atlas of Living Australia database (available via ALA4R)
 #ALA4R is a package that includes taxonomic information for all Australian species, including plants.
-#More details on ALA4R can be found here https://atlasoflivingaustralia.github.io/ALA4R/articles/ALA4R.html
+#More details on ALA4R can be found at https://atlasoflivingaustralia.github.io/ALA4R/articles/ALA4R.html
 
-#To begin, upload the most recent version of ausplotsR
+#open most recent version of ausplotsR
 library(devtools)
 install_github("ternaustralia/ausplotsR", build_vignettes = FALSE)
 library(ausplotsR)
 
-#Open addtional Libraries 
+#Open additional libraries 
+library(ALA4R)
 library(rgbif)
 library(data.table)
 library(vegan)
@@ -24,13 +26,11 @@ library(car)
 library(dplyr)
 library(tidyr)
 library(reshape)
-library(ALA4R)
 library(stringr)
 
-#upload ausplots data
+#import ausplots data
 my.ausplots.data <- get_ausplots()
-#get_ausplots can be used to filter the species search list based on plots, areas, or species
-#run ?get_ausplots for details.
+#get_ausplots() can be used to filter the species search list based on plots, areas, or species
 #This example uses the entire ausplots species list
 
 #extract all species names from TERN Database, this can be done using any ausplotsR dataframe that includes $herbarium_determination, in this case I am using veg.vouch
@@ -56,23 +56,24 @@ species_names_1$family <- NA
 #create an empty column to save common name search results
 species_names_1$common_name <- NA 
 
-#next we search the species name to get the family
+#search the species name to get the family
 #for each unique genus, use ALA4R to extract the family
 for(i in unique(species_names_1$genus)){
   si<-species_info(i)#retrieve taxa info for each unique genus
   species_names_1[which(species_names_1$genus == i),]$family<-si$classification$family
 } # note that this loop can be modified to extract any relavent piece of species information available through ALA
 
-#ALA will help ID a lot of families, however, ALA doesn't have taxonomic info for all genera
-#taxonmic names are always changing, which can cause issues
-#Moreover, not every TERN entry will have a genus (some records are only indentified to the family or even growth form level, or ids may be pending)
+#ALA will ID a lot of families, however, ALA doesn't have all taxonomic info for all genera
+#names are always changing, which can cause issues
+#Moreover, not every TERN entry will have a genus 
+#some records are only indentified to the family or even growth form level
 #in cases where no family is retrieved, the species_names_1$family row will be assigned NA
-#to find which species could not be assigned a family, we simply extract all the NAs in the family column
+#to find which species could not be assigned a family, extract all the NAs in the family column
 
 NA_families<- species_names_1[is.na(species_names_1$family),]
 missing_names<-unique(NA_families$herbarium_determination)
 
-# to fill in these gaps, we can try a second package called rgbif 
+# to fill in these gaps, use a second package called rgbif 
 #this package is provided by the Global Biodiversity Information Facility ('GBIF'; <https://www.gbif.org/developer/summary>). 
 #'GBIF' is a database of species occurrence records from sources all over the globe. 
 #for each missing name, the correct family can be searched manually using the rgbif package
@@ -86,21 +87,22 @@ species_names_1[which(species_names_1$genus == "Euryomytus"),]$family<-"Myrtacea
 species_names_1[which(species_names_1$genus == "Euryomytus"),]$family<- familiy_name$family
 
 
-#next we can use ALA to search for species common names
+#next use ALA to search for species common names
 #for each unique species, use ALA4R to extract the common name
 for(i in unique(species_names_1$herbarium_determination)){
   si<-species_info(i)#retrieve taxa info for each unique genus
   species_names_1[which(species_names_1$herbarium_determination == i),]$common_name<-si$commonNames$nameString[1] #extracts only the first common name provided by ALA
 }
-#Note that common names are often not available for plant species, and that this search, depending on the size of the species list, can also be a bit slow
+#Note that common names are often not available for plant species
+#depending on the size of the species list, can also be a bit slow
 
 #ALA will sometimes assign the scientific name as the common name if the actual common name is not known/recorded in the ALA database
-#this could be confusing, so we want to convert common names that are actually scientific names to NAs to indicate there is no known/recorded common name for that species
-
+#this could be confusing, so convert common names that are actually scientific names to NAs to indicate there is no known/recorded common name for that species
+#in cases where common names and species names are indentical, convert common name to NA
 species_names_1 <- species_names_1 %>% 
-  mutate(common_name = ifelse(herbarium_determination != common_name, common_name, NA)) #in cases where common names and species names are indentical, convert common name to NA
+  mutate(common_name = ifelse(herbarium_determination != common_name, common_name, NA))
 
-#finally, save the output
+#save the output
 write.csv(species_names_1, file="species_names.csv")##
 
 ###END#####
